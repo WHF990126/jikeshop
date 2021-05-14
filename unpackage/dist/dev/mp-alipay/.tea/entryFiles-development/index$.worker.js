84d1264f212dd2c4dc746642952eeb7e929b4fff
WHF990126 <859912013@qq.com>
2021-05-14 14:55:50 +0800

    完成网络请求的封装和tabbar的配置

--- unpackage/dist/dev/mp-alipay/.tea/entryFiles-development/index$.worker.js
+++ unpackage/dist/dev/mp-alipay/.tea/entryFiles-development/index$.worker.js
@@ -37,6 +37,9 @@ function success() {
 require('../../app');
 require('../../components/uni-link/uni-link?hash=ec7c5687c7f2ffa836caf3a93e877914426baff6');
 require('../../pages/index/index?hash=2d09a75b27a6cba8ec6b6d9029208ec9191e8c9b');
+require('../../pages/news/news?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../pages/member/member?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../pages/cart/cart?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
 }
 self.bootstrapApp ? self.bootstrapApp({ success }) : success();
 }

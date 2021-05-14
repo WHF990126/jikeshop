a4930de25648ab443456439b0dd9f2ae92fa5067
WHF990126 <859912013@qq.com>
2021-05-14 14:24:32 +0800

    初始化项目

---
+++ unpackage/dist/dev/mp-alipay/.tea/entryFiles-development/index$.web.js
@@ -0,0 +1,5 @@
+require('@alipay/appx-compiler/lib/sjsEnvInit');
+require('./config$');
+
+require('../../components/uni-link/uni-link?hash=ec7c5687c7f2ffa836caf3a93e877914426baff6');
+require('../../pages/index/index?hash=2d09a75b27a6cba8ec6b6d9029208ec9191e8c9b');
---
+++ unpackage/dist/dev/mp-alipay/.tea/entryFiles-development/index$.worker.js
@@ -0,0 +1,42 @@
+if(!self.__appxInited) {
+self.__appxInited = 1;
+
+
+require('./config$');
+
+
+      function getUserAgentInPlatformWeb() {
+        return typeof navigator !== 'undefined' ? navigator.swuserAgent || navigator.userAgent || '' : '';
+      }
+      if(getUserAgentInPlatformWeb() && (getUserAgentInPlatformWeb().indexOf('LyraVM') > 0 || getUserAgentInPlatformWeb().indexOf('AlipayIDE') > 0) ) {
+        var AFAppX = self.AFAppX.getAppContext ? self.AFAppX.getAppContext().AFAppX : self.AFAppX;
+      } else {
+        importScripts('https://appx/af-appx.worker.min.js');
+        var AFAppX = self.AFAppX;
+      }
+      self.getCurrentPages = AFAppX.getCurrentPages;
+      self.getApp = AFAppX.getApp;
+      self.Page = AFAppX.Page;
+      self.App = AFAppX.App;
+      self.my = AFAppX.bridge || AFAppX.abridge;
+      self.abridge = self.my;
+      self.Component = AFAppX.WorkerComponent || function(){};
+      self.$global = AFAppX.$global;
+      self.requirePlugin = AFAppX.requirePlugin;
+    
+
+if(AFAppX.registerApp) {
+  AFAppX.registerApp({
+    appJSON: appXAppJson,
+  });
+}
+
+
+
+function success() {
+require('../../app');
+require('../../components/uni-link/uni-link?hash=ec7c5687c7f2ffa836caf3a93e877914426baff6');
+require('../../pages/index/index?hash=2d09a75b27a6cba8ec6b6d9029208ec9191e8c9b');
+}
+self.bootstrapApp ? self.bootstrapApp({ success }) : success();
+}
